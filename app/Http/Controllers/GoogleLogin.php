f2c848db882b18382cab1c412e07db37f1ebcd04
gerardoGP <gerardo210299@gmail.com>
2023-10-24 17:15:31 -0500

    primera version

---
+++ app/Http/Controllers/GoogleLogin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+
+class GoogleLogin extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    public function handleGoogleCallback()
+    {
+        echo "OK";
+        // try {
+        //     $user = Socialite::driver('google')->user();
+        //     $finduser = User::where('google_id', $user->id)->first();
+        //     if($finduser){
+        //         Auth::login($finduser);
+        //         return redirect('/home');
+        //     }else{
+        //         $newUser = User::create([
+        //             'name' => $user->name,
+        //             'email' => $user->email,
+        //             'google_id'=> $user->id
+        //         ]);
+        //         Auth::login($newUser);
+        //         return redirect()->back();
+        //     }
+        // } catch (Exception $e) {
+        //     return redirect('auth/google');
+        // }
+    }
+}
---
+++ database/migrations/2023_10_24_202358_create_postulants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostulantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postulants', function (Blueprint $table) {
+            $table->id();
+            $table->string("name","100");
+            $table->string("nro_ruc","20");
+            $table->string("nro_phone","20");
+            $table->string("mail","50");
+            $table->foreignId("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
